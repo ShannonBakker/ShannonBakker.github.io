d14902b6c425bab1afdecdf789d5944cb1979421
ShannonBakker <shannonc2m@live.nl>
2016-06-30 14:56:11 +0200

    laatste versie

--- javascript/afstand_visualisatie.js
+++ javascript/afstand_visualisatie.js
@@ -1,11 +1,16 @@
+// make a horizontal barchart
+// Shannon Bakker 11201401
+
 function visualisation_distance(){
 	// color the map for Terschelling
 	colour_map(321,3, 2014);
 
+	// set the margins
 	var margin = {top: 80, right: 70, bottom: 120, left: 150},
 		width = 600 - margin.left - margin.right,
 		height = 600 - margin.top - margin.bottom;
 
+	// set the x and y axis
 	var y = d3.scale.ordinal()
 		.rangeRoundBands([0, width], 0.1);
 
@@ -28,7 +33,7 @@ function visualisation_distance(){
 	d3.json("afstanden.json", function(error, data){
 		if (error) {
 			alert("Er is iets misgegaan met het laden van de data");
-			throw new Error("Something went badly wrong!");
+			throw new Error;
 		};
 		y.domain(data.data.map(function(d){ return d.plaatsnaam + " ("+ d.provincie+")"}));
 		x.domain([0, d3.max(data.data, function(d){return  d.afstand})]);
@@ -49,27 +54,26 @@ function visualisation_distance(){
 			.attr("transform","translate(150,0)")
 			.call(yAxis);
 
-		// append the bars 
+		// append the text of the bars 
 		chart.selectAll(".bar")
 			.data(data.data)
 			.enter().append("g")
-				.attr("class", "bar1")
-				.attr("font-size", "10px")
-				.append("rect")
-					.attr("class", "bar")
-					.attr("y", function(d) { return y(d.plaatsnaam + " ("+ d.provincie+")"); })
-					.attr("height", y.rangeBand())
-					.attr("x", function(d) { return 150; })
-					.attr("width", function(d) { return x(d.afstand); })
-					.on('mouseover', function(d){colour_map(d.gemeentenummer, 3, 2014)});
-		
-		// append text to the bars
-		d3.selectAll('.bar1')
-			.append("text")
-				.attr("class","bartext")
-				.attr("x", function(d) { return x(d.afstand)+150-40; })
-				.attr("y",  function(d) { return y(d.plaatsnaam + " ("+ d.provincie+")")+17; })
-				.attr("dy", ".35em")
-				.text(function(d) { return d.afstand + ' km'; });
+				.attr("class", "bar_container")
+				.append("text")
+					.attr("class","bartext")
+					.attr("x", function(d) { return x(d.afstand)+150-40; })
+					.attr("y",  function(d) { return y(d.plaatsnaam + " ("+ d.provincie+")")+17; })
+					.attr("dy", ".35em")
+					.text(function(d) { return d.afstand + ' km'; });
+				
+		// append the bars
+		d3.selectAll('.bar_container')
+			.append("rect")
+				.attr("class", "bar")
+				.attr("y", function(d) { return y(d.plaatsnaam + " ("+ d.provincie+")"); })
+				.attr("height", y.rangeBand())
+				.attr("x", 150)
+				.attr("width", function(d) { return x(d.afstand); })
+				.on('mouseover', function(d){colour_map(d.gemeentenummer, 3, 2014)});		
 	});
 };
